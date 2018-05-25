c8622fb8896b3315c1d459fa6b0f8df93bf29a20
Unknown <uoshvis@gmail.com>
2018-05-25 15:52:09 +0300

    dev settings add browsableAPI db dir change

--- number-plate-app/car_plates_api/settings/development.py
+++ number-plate-app/car_plates_api/settings/development.py
@@ -5,6 +5,14 @@ DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # use old DB; in parent dir
+        'NAME': os.path.join(BASE_DIR, '..', 'db.sqlite3'),
     }
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    )
 }
