647e4e282aec0a710272b98092742fdf765098c0
Unknown <uoshvis@gmail.com>
2018-08-12 20:25:50 +0300

    change facts string length

--- number-plate-app/facts/models.py
+++ number-plate-app/facts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Fact(models.Model):
-    fact = models.CharField(max_length=200)
+    fact = models.CharField(max_length=400)
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def _str_(self):
