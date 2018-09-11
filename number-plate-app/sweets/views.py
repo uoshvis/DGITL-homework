9f232828a752da03b24007ea7b65a4936254cd1b
Unknown <uoshvis@gmail.com>
2018-09-11 20:55:08 +0300

    remove post in sweets

--- number-plate-app/sweets/views.py
+++ number-plate-app/sweets/views.py
@@ -6,7 +6,8 @@ from sweets.serializers import SweetsSerializer
 from datetime import datetime
 
 
-class SweetsDataAPIView(mixins.CreateModelMixin, generics.ListAPIView):
+
+class SweetsDataAPIView(generics.ListAPIView):
 
     lookup_field = 'pk'
     serializer_class = SweetsSerializer
@@ -21,11 +22,33 @@ class SweetsDataAPIView(mixins.CreateModelMixin, generics.ListAPIView):
             )
         return qs
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+    # def post(self, request, *args, **kwargs):
+    #     return self.create(request, *args, **kwargs)
 
     def get_serializer_context(self, *args, **kwargs):
         return {'request': self.request}
+
+
+# class SweetsDataAPIView(mixins.CreateModelMixin, generics.ListAPIView):
+
+#     lookup_field = 'pk'
+#     serializer_class = SweetsSerializer
+
+#     def get_queryset(self):
+#         qs = SweetsData.objects.all()
+#         query_timestamp = self.request.GET.get('timestamp')
+#         if query_timestamp and query_timestamp.isdigit():
+#             timestamp = datetime.fromtimestamp(float(query_timestamp))
+#             qs = qs.filter(
+#                 Q(timestamp__gte=timestamp)
+#             )
+#         return qs
+
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
+
+#     def get_serializer_context(self, *args, **kwargs):
+#         return {'request': self.request}
 
 
 class SweetsDataRudView(generics.RetrieveUpdateDestroyAPIView):
